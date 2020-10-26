02da71bc25ccf865d4544e7c36fbbc444aaac7b2
reneweiser <rene@encoding.group>
2020-10-26 09:32:45 +0100

    Removed adorable.io and replaced it with gravatar defaults.

--- app/helpers.php
+++ app/helpers.php
@@ -5,7 +5,8 @@ use Illuminate\Support\Str;
 
 function gravatar_url($user)
 {
-    return 'https://s.gravatar.com/avatar/' . md5($user->email) . '?s=60&d=https://api.adorable.io/avatars/60/' . md5($user->name);
+    return 'https://s.gravatar.com/avatar/' . md5($user->email) . '?s=60&d=identicon';
+//    return 'https://s.gravatar.com/avatar/' . md5($user->email) . '?s=60&d=https://api.adorable.io/avatars/60/' . md5($user->name);
 }
 
 function str_limit($string, $limit) {
